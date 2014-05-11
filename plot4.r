0574e017309e30edc807d513d5d054d7ca5937f9
mimaima <mimaima@users.noreply.github.com>
2014-05-11 15:58:10 -0500

    read data by specifying the dates

--- plot4.r
+++ plot4.r
@@ -1,27 +1,35 @@
 # 05/10/2014
-# Course Project 1 related to the course "Exploratory Data Analysis" of Coursera.
+# Exploratory Data Analysis Course Project 1
 # plot4
 
+# if you do not have sqldf package, you can install sqldf package by running the following command: install.packages("sqldf")
+require(sqldf)
+
+# clean all objects in R memory
 rm(list=ls())
-#reading data into R (you need to put the data in a sub-directory named on "Explor"):
-data<- read.table("./household_power_consumption.txt", sep=";",nrows= 2075259, header=TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
 
-# Subsetting the full data to obtain the data related to two days: 
-subdata<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
+# read 1/2/2007 and 2/2/2007 data from household_power_consumption.txt in the same directory
+file <- "./household_power_consumption.txt"
+data <- read.csv.sql(file, sql = "select * from file where Date in ('1/2/2007','2/2/2007')", header = TRUE, sep = ";")
 
-# Changing the class of Date variable from character to Date: 
-subdata$Date <- as.Date(subdata$Date, format = "%d/%m/%Y")
-# Combining the Date and Time variable and creating a new column in dataset named "DateTime":
-subdata$DateTime <- as.POSIXct(paste(subdata$Date, subdata$Time))
+# Change Date variable from String to Date
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+# Create a new column DateTime from Date and Time
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
 
-# Creating the plot4:
+# create plot4.png
 png("plot4.png", width = 480, height = 480)
-par(mfcol=c(2,2)) # adding plots in col-wised manner.
-plot(subdata$DateTime, subdata$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="") # Adding the first plot.
-plot(subdata$DateTime, subdata$Sub_metering_1, type="l", ylab= "Energy sub metering", xlab="")
-lines(subdata$DateTime, subdata$Sub_metering_2, type="l", col="red")
-lines(subdata$DateTime, subdata$Sub_metering_3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, bty="n", col=c("black", "red", "blue")) # The end of adding the second plot.
-plot(subdata$DateTime, subdata$Voltage, type="l", xlab="datetime", ylab="Voltage")   # Adding the third plot.
-plot(subdata$DateTime, subdata$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")  # Adding the forth plot.
+# define 4 curves in one plot and add plots in the order of (1,1), (1,2), (2,1), (2,2)
+par(mfrow=c(2,2))
+# 1st plot
+plot(data$DateTime, data$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="")
+# 2nd plot
+plot(data$DateTime, data$Voltage, type="l", xlab="datetime", ylab="Voltage")
+# 3rd plot
+plot(data$DateTime, data$Sub_metering_1, type="l", ylab= "Energy sub metering", xlab="")
+lines(data$DateTime, data$Sub_metering_2, type="l", col="red")
+lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, bty="n", col=c("black", "red", "blue"))
+# 4th plot
+plot(data$DateTime, data$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 dev.off()
